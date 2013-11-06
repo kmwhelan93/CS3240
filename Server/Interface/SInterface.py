1abc9bcac38aef6e08f282550e7eaaeece4b1b47
juscing <juscing@gmail.com>
2013-11-06 01:54:10 -0500

    More GUI changes. Start the interface with start_Server_Interface.py

--- Server/Interface/SInterface.py
+++ Server/Interface/SInterface.py
@@ -3,31 +3,71 @@ __author__ = 'justin'
 from Tkinter import *
 from ttk import Frame, Style
 from Server import DbOps
+from idlelib.WidgetRedirector import WidgetRedirector
+
+
+class ReadOnlyText(Text):
+    def __init__(self, *args, **kwargs):
+        Text.__init__(self, *args, **kwargs)
+        self.redirector = WidgetRedirector(self)
+        self.insert = self.redirector.register("insert", lambda *args, **kw: "break")
+        self.delete = self.redirector.register("delete", lambda *args, **kw: "break")
 
 class View(Frame):
-    def __init__(self, parent):
+    def __init__(self, parent, govnah):
+        self.govnah = govnah
         Frame.__init__(self, parent)
         self.parent = parent
         self.initUI()
 
     def initUI(self):
         self.parent.title("OneDir Server Admin Interface")
-        self.pack(fill=BOTH, expand=1)
-        message = Label(self.parent, text="Hello")
-        message.pack()
-        w = Text(self.parent, bg="white", undo=False, state='disabled')
-        w.pack()
-        w.insert(END, "hello")
+        #self.pack(fill=BOTH, expand=1)
+        commands = Label(self.parent, text="Commands")
+        commands.grid(row=0, columnspan=3)
+        output = Label(self.parent, text="Output")
+        output.grid(row=0, column=3)
 
-        #w.insert("hello")
+        scrollbar = Scrollbar(self.parent, )
+        scrollbar.grid(row=1, column=4, rowspan=100, sticky=N+S)
+
+        self.log = ReadOnlyText(self.parent, bg="white", undo=False, yscrollcommand=scrollbar.set, width=120)
+        self.log.grid(row=1, column=3, rowspan=100)
+
+        scrollbar.config(command=self.log.yview)
+
+        userStuff = Label(self.parent, text="List Users: ")
+        userStuff.grid(row=1, column=0)
+
+        printUserName = Button(self.parent, text="By Name", command=self.govnah.printUsersByName)
+        printUserName.grid(row=1, column=1)
+
+        printUserTime = Button(self.parent, text="By Time", command=self.govnah.printUsersByName)
+        printUserTime.grid(row=1, column=2)
+
+        trans = Label(self.parent, text="List Transactions: ")
+        trans.grid(row=2, column=0)
+
+        printTransType = Button(self.parent, text="By Name")
+        printTransType.grid(row=2, column=1)
+
+        printTransTime = Button(self.parent, text="By Time")
+        printTransTime.grid(row=2, column=2)
+
+        self.log.insert(END, "Howdy Admin, Welcome to the Server Interface \n\n")
+
+    def appendText(self, text):
+        self.log.insert(END, str(text) + "\n")
+        self.log.see(END)
+
 
 class SInterface():
 
     def __init__(self):
         self.db = DbOps.DbOps()
         root = Tk()
-        root.geometry("250x250+300+300")
-        v = View(root)
+        root.geometry("1150x400+100+100")
+        self.view = View(root, self)
         root.mainloop()
 
     def menu(self):
@@ -41,6 +81,24 @@ class SInterface():
         print "6. Start Server Daemon"
         print "0. Exit"
         print "-----------------------------------------\n"
+
+    def printUsersByName(self):
+        self.view.appendText("The list of users, sorted by username. (Unique ID, username, password hash, registration timestamp)")
+        self.printSanitizeDBstr(self.db.getUsersByUName())
+
+    def printUsersByTime(self):
+        self.view.appendText("The list of users, sorted by registration time. (Unique ID, username, password hash, registration timestamp)")
+        self.printSanitizeDBstr(self.db.getUsersByTime())
+
+    def printSanitizeDBstr(self, results):
+        for entry in results:
+            t = ""
+            for item in entry:
+                t = t + str(item) + "\t"
+            self.view.appendText(t)
+        self.view.appendText("")
+
+
 
     def start(self):
         while True:
