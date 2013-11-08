b13c592d83b2de5962b78d6d7f27be08ccefd576
Venkat Muruganandam <2011vmurugan@gmail.com>
2013-11-08 16:55:19 -0500

    Added event handler

--- get_modification.py
+++ get_modification.py
@@ -1,5 +1,42 @@
 import os
 import datetime
+import sys
+import time
+import logging
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+
+class SyncEventHandler(FileSystemEventHandler):
+  """Logs all the events captured."""
+
+  def on_moved(self, event):
+    super(SyncEventHandler, self).on_moved(event)
+
+    what = 'directory' if event.is_directory else 'file'
+    logging.info("Moved %s: from %s to %s", what, event.src_path,
+                 event.dest_path)
+
+  def on_created(self, event):
+    super(SyncEventHandler, self).on_created(event)
+
+    what = 'directory' if event.is_directory else 'file'
+    logging.info("Created %s: %s", what, event.src_path)
+
+  def on_deleted(self, event):
+    super(SyncEventHandler, self).on_deleted(event)
+
+    what = 'directory' if event.is_directory else 'file'
+    logging.info("Deleted %s: %s", what, event.src_path)
+
+  def on_modified(self, event):
+    super(SyncEventHandler, self).on_modified(event)
+
+    what = 'directory' if event.is_directory else 'file'
+    logging.info("Modified %s: %s", what, event.src_path)
+
+
+
 
 def modification_date(base_path, filename):
     t = os.path.getmtime(os.path.join(base_path, filename))
@@ -7,7 +44,28 @@ def modification_date(base_path, filename):
     #return datetime.datetime.fromtimestamp(t)
 
 def modification_dir(base_path):
-	for file in os.listdir(base_path):
-		print file, modification_date(base_path, file)
+    timestamps.clear()
+    for file in os.listdir(base_path):
+        modTime = modification_date(base_path, file)
+        timestamps[file] = modTime
+    print timestamps
 
-modification_dir("/home/venkat/Documents")
+def watchDog(base_path):
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    event_handler = SyncEventHandler()
+    observer = Observer()
+    observer.schedule(event_handler, base_path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
+
+timestamps = dict()
+path = sys.argv[1] if len(sys.argv) > 1 else '.'
+modification_dir(path)
+watchDog(path)
