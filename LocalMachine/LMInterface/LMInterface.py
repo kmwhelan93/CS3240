600fa0cf41caeab53b4833b21e35ecf5cfc1a358
mgg9xv <mgg9xv@virginia.edu>
2013-11-18 00:59:17 -0500

    Added sign up functionality to login scree

--- LocalMachine/LMInterface/LMInterface.py
+++ LocalMachine/LMInterface/LMInterface.py
@@ -25,32 +25,50 @@ class Gui(Frame):
     def loginWindow(self):
 
         self.lock = True
-        self.top = Toplevel( padx=10, pady = 10)
+        self.top = Toplevel ( self, padx = 10, pady = 10 )
 
         self.top.protocol('WM_DELETE_WINDOW', self.closeWindow)
         self.top.title("Login to OneDir Preferences")
+
         self.top.loginstatus_var = StringVar()
-        self.top.loginstatus_var.set("Enter you Username and Password Above")
+        self.top.loginstatus_var.set("Enter you username and password below to login.")
 
-        usernameLabel = Label(self.top, text="Username:", padx=5, pady = 5).grid(row=0, columnspan=1)
-        passwordLabel = Label(self.top, text="Password:", padx=5, pady = 5).grid(row=1, columnspan=1)
+        self.top.login_entry= Label(self.top, padx=5, pady = 5,
+                                    wraplength = 300, textvariable=self.top.loginstatus_var)
+        self.top.login_entry.grid(row=0,column=0,columnspan=3)
 
-        self.usernameE= Entry(self.top , width = 30)
-        self.usernameE.grid(row=0,column=1,columnspan=2)
-        self.passwordE = Entry(self.top, width = 30, show="*")
-        self.passwordE.grid(row=1,column=1,columnspan=2)
+        usernameL = Label(self.top, text="Username:", padx=5, pady = 5)
+        usernameL.grid(row=1, columnspan=1)
+        passwordL = Label(self.top, text="Password:", padx=5, pady = 5)
+        passwordL.grid(row=2, columnspan=1)
 
-        self.top.status_entry= Label(self.top, padx=5, pady = 5, textvariable=self.top.loginstatus_var)
-        self.top.status_entry.grid(row=2,column=0,columnspan=3)
+        self.usernameE= Entry(self.top , width = 40)
+        self.usernameE.grid(row=1,column=1,columnspan=2)
+        self.passwordE = Entry(self.top, width = 40, show="*")
+        self.passwordE.grid(row=2,column=1,columnspan=2)
 
-        SignUpBtn = Button(self.top, text="Sign Up", command=self.closeWindow)
-        SignUpBtn.grid(row=3, column = 0, columnspan =1)
+        LoginBtn = Button(self.top, text="Login", width = 40,
+                          command=lambda:self.login(self.usernameE.get(),self.passwordE.get()))
+        LoginBtn.grid(row=3 ,column = 0, columnspan = 3)
 
-        closeBtn = Button(self.top, text="Cancel", command=self.closeWindow)
-        closeBtn.grid(row=3, column = 1, columnspan =1)
 
-        LoginBtn = Button(self.top, text="Login", command=lambda:self.login(self.usernameE.get(),self.passwordE.get()))
-        LoginBtn.grid(row=3 ,column = 2, columnspan = 1)
+        self.top.signupstatus_var = StringVar()
+        self.top.signupstatus_var.set("To set up account, please enter desired OneDir path,"
+                                      " along with your desired username and password. ")
+
+        self.top.signup_entry= Label(self.top, padx=5, pady = 5,
+                                     wraplength = 300, textvariable=self.top.signupstatus_var)
+        self.top.signup_entry.grid(row=4,column=0,columnspan=3)
+
+        directoryL = Label(self.top, text="Directory:", padx=5, pady = 5)
+        directoryL.grid(row=5, column = 0, columnspan=1)
+
+        self.directoryE= Entry(self.top , width = 40)
+        self.directoryE.grid(row=5,column=1,columnspan=2)
+
+        SignUpBtn = Button(self.top, text="Sign Up" , width = 40,
+                command=lambda:self.signup(self.usernameE.get(),self.passwordE.get(),self.directoryEget()))
+        SignUpBtn.grid(row=6, column = 0, columnspan =3)
 
 
         self.center(self.top)
@@ -77,6 +95,7 @@ class Gui(Frame):
         username_entry= Label(self.parent,textvariable=username_var).grid(row=0,column=1,columnspan=2)
         password_entry= Label(self.parent, textvariable=password_var).grid(row=1,column=1,columnspan=2)
         directory_entry= Label(self.parent, textvariable=directory_var).grid(row=2,column=1,columnspan=2)
+
         autosyncCheck = Checkbutton(self.parent, text="Select to enable Autosync", variable=autosync_var)
         autosyncCheck.grid(row=3,column=1,columnspan=2)
 
@@ -91,23 +110,34 @@ class Gui(Frame):
 
     def login(self, username, password):
         if ( self.govnah.ops.userExists(username) == False):
-            self.top.loginstatus_var.set("No record of this username on this machine.")
-            self.top.status_entry.config(fg="blue")
+            self.top.loginstatus_var.set("No record of this username on this machine. You must set up your directory first.")
+            self.top.login_entry.config(fg="blue")
+            self.top.signupstatus_var.set("Enter a valid directory path to go along with desired username and  password. ")
+            self.top.signup_entry.config(fg="blue")
             return
 
         if self.govnah.ops.authUser(username,password):
             self.top.loginstatus_var.set("Username and password  match.")
-            self.top.status_entry.config(fg="green")
+            self.top.login_entry.config(fg="green")
             self.closeWindow()
             self.parent.deiconify()
             return
 
         self.top.loginstatus_var.set("Username and password don't match.")
-        self.top.status_entry.config(fg="red")
+        self.top.login_entry.config(fg="red")
         return
 
-    def usernameCallBack(self,event):
-        self.depositLabel.config(text='change the value')
+    def signup(self, username, password, directory):
+        if ( self.govnah.ops.userExists(username) == True):
+            self.top.loginstatus_var.set("That username has already been taken. Try another.")
+            self.top.login_entry.config(fg="red")
+            self.top.signupstatus_var.set("Enter a valid directory path to go along with desired username and password. ")
+            self.top.signup_entry.config(fg="red")
+            return
+
+        return
+
+
 
     def getNewPw(self):
         if self.govnah.ops.authenticateUser:
