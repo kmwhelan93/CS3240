d6cb7cf0db677e3cd41222807e57da17f8d208d0
juscing <juscing@gmail.com>
2013-12-03 12:52:22 -0500

    Starting a new client interface should with the preference set to not auto sync should kill an already running client daemon.

--- LocalMachine/preferences_Operations.py
+++ LocalMachine/preferences_Operations.py
@@ -164,3 +164,14 @@ class preferenceOperations:
     def checkStartDaemon(self, username):
         if self.getAutoSyncSetting(username):
             subprocess.Popen(["python", "LocalMachine/LMDaemon/client_daemon.py", "--path", self.getDirectoryPath(username)])
+        else:
+            ps = subprocess.Popen("ps -ef | grep client_daemon.py | grep -v grep", shell=True, stdout=subprocess.PIPE)
+            output = ps.stdout.read()
+            ps.stdout.close()
+            ps.wait()
+            if len(output) > 3:
+                procstring = output.split()
+                pid = procstring[1]
+                print int(pid)
+                print os.kill(int(pid), signal.SIGKILL)
+            print "AutoSync setting has been turned off."
