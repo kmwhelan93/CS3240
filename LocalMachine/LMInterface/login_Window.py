d5cb485ba281d4e7bd5a43876e1868feb8c1f145
juscing <juscing@gmail.com>
2013-12-04 14:07:33 -0500

    Change client interface to support network stuff

--- LocalMachine/LMInterface/login_Window.py
+++ LocalMachine/LMInterface/login_Window.py
@@ -6,6 +6,8 @@ class loginWindow:
 
     def __init__(self, clientInterface):
 
+        self.lock = False
+
         self.UI = clientInterface
 
         self.lwin = Toplevel(self.UI.root,  padx=10, pady=10)
@@ -24,13 +26,13 @@ class loginWindow:
         self.passwordLabel = Label(self.lwin, text="Password:", padx=5, pady=5)
         self.passwordLabel.grid(row=2, columnspan=1)
 
-        self.usernameEntry = Entry(self.lwin, width=40, bg="white")
+        self.usernameEntry = Entry(self.lwin, width=40, bg="white", validate=ALL, validatecommand=self.validateEdit)
         self.usernameEntry.grid(row=1, column=1, columnspan=2)
-        self.passwordEntry = Entry(self.lwin, width=40, show="*", bg="white")
+        self.passwordEntry = Entry(self.lwin, width=40, show="*", bg="white", validate=ALL, validatecommand=self.validateEdit)
         self.passwordEntry.grid(row=2, column=1, columnspan=2)
 
         self.loginButton = Button(self.lwin, text="Login", width=40,
-                          command=lambda:self.login(self.usernameEntry.get(),self.passwordEntry.get()))
+                          command=lambda:self.login(self.usernameEntry.get(),self.passwordEntry.get(),self.directoryEntry.get()))
         self.loginButton.grid(row=3, column=0, columnspan=3)
 
         self.setUpText = StringVar()
@@ -44,7 +46,7 @@ class loginWindow:
         self.directoryLabel = Label(self.lwin, text="Directory:", padx=5, pady=5)
         self.directoryLabel.grid(row=5, column=0, columnspan=1)
 
-        self.directoryEntry = Entry(self.lwin, width=40, bg="white")
+        self.directoryEntry = Entry(self.lwin, width=40, bg="white", validate=ALL, validatecommand=self.validateEdit)
         self.directoryEntry.grid(row=5, column=1, columnspan=2)
 
         self.setUpButton = Button(self.lwin, text="Sign Up", width=40,
@@ -54,59 +56,81 @@ class loginWindow:
         self.ipLabel = Label(self.lwin, text="Server IP:")
         self.ipLabel.grid(row = 7, column=0)
 
-        self.ipEntry = Entry(self.lwin, width=15, bg="white")
+        self.ipEntry = Entry(self.lwin, width=15, bg="white", validate=ALL, validatecommand=self.validateEdit)
         self.ipEntry.grid(row=7, column=1, columnspan=2)
 
         self.UI.center(self.lwin)
 
+    def validateEdit(self):
+        return not self.lock
 
-    def login(self, username, password):
+    def login(self, username, password, directory):
+        self.username = username
+        self.password = password
+        self.directory = directory
+        if not self.UI.prefOps.userExistsLocally(username) and not self.directory:
+            self.loginText.set("No record of this username on this machine. Choose your OneDir directory for this machine.")
+            self.loginStatus.config(fg="blue")
+            self.setUpText.set("Click 'Login' again after specifying directory to set up your OneDir directory on this machine.")
+            self.setUpStatus.config(fg="blue")
+        else:
+            self.lock = True
+            #Create and Login object here
 
-        if ( self.UI.prefOps.userExistsGlobally(username) == False ):
+    def loginResponse(self, response):
+        if response["status"] == "NOT_EXIST":
             self.loginText.set("No record of this username on our servers. Choose your OneDir username, password, and directory.")
             self.loginStatus.config(fg="blue")
             self.setUpText.set("Click 'Set Up' to create your OneDir account and set up your OneDir directory on this machine. ")
             self.setUpStatus.config(fg="blue")
-            return
-
-        elif ( self.UI.prefOps.userExistsLocally(username) == False):
-            self.loginText.set("No record of this username on this machine. Choose your OneDir directory for this machine.")
-            self.loginStatus.config(fg="blue")
-            self.setUpText.set("Click 'Set Up' to set up your OneDir directory on this machine.")
-            self.setUpStatus.config(fg="blue")
-            return
-
-        elif ( (self.UI.prefOps.userExistsLocally(username) == True) & (self.UI.prefOps.authenticateUser(username, password) == False)):
+        if response["status"] == "WRONG_PASSWORD":
             self.loginText.set("The username and password you have entered do not match. Please try again. ")
             self.loginStatus.config(fg="red")
             self.setUpText.set("Click 'Log In' to login to you OneDir Preferences. No need to specify a new directory. ")
             self.setUpStatus.config(fg="red")
-            return
-
-        elif self.UI.prefOps.authenticateUser(username, password):
-            self.UI.passwordLength = len(password)
-            self.UI.initUI(username, password)
+        if response["status"] == "SUCCESS":
+            if not self.UI.prefOps.userExistsLocally(self.username):
+                self.UI.prefOps.createUser(self.username, self.password, self.directory)
+            self.UI.passwordLength = len(self.password)
+            self.UI.initUI(self.username, self.password)
             self.closeWindow()
             return
 
+        self.lock = False
 
     def signup(self, username, password, directory):
+        self.username = username
+        self.password = password
+        self.directory = directory
 
-        if ( self.UI.prefOps.validDirectoryPath(directory) == True) & (len(password) > 0):
-            self.UI.prefOps.createUser(username, password, directory)
+        if not len(password) > 4:
+            self.loginText.set("Choose a longer password")
+            self.loginStatus.config(fg="red")
+
+        if not self.UI.prefOps.validDirectoryPath(directory):
+            self.setUpText.set("Enter a valid directory path to go along with desired username and password.")
+            self.setUpStatus.config(fg="red")
+
+        if len(password) > 4 and self.UI.prefOps.validDirectoryPath(directory):
+            self.lock = True
+            #CREATE AND SEND OBJECT
+
+    def signupResponse(self, response):
+
+        #RECEIVE OBJECT CAN BE BOOLEAN
+
+        if(response):
+            self.UI.prefOps.createUser(self.username, self.password, self.directory)
             self.setUpText.set("You have successfully set up you OneDir directory on this machine.")
             self.setUpStatus.config(fg="green")
-            self.UI.passwordLength = len(password)
-            self.UI.initUI(username, password)
+            self.UI.passwordLength = len(self.password)
+            self.UI.initUI(self.username, self.password)
             self.closeWindow()
-            return
-
-        if self.UI.prefOps.userExistsLocally(username):
+        else:
             self.loginText.set("That username has already been taken. Try another.")
             self.loginStatus.config(fg="red")
-            self.setUpText.set("Enter a valid directory path to go along with desired username and password. ")
-            self.setUpStatus.config(fg="red")
-            return
+
+        self.lock = False
 
     def closeWindow(self):
         self.lwin.destroy()
