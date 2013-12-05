8272551fe7a4507a7a26ea87410369e759f63343
vmuruganandam <2011vmurugan@gmail.com>
2013-12-04 21:04:50 -0500

    get directory size in common

--- common.py
+++ common.py
@@ -92,6 +92,14 @@ def get_timestamps(base_path):
         timestamps.pop(path, timestamps[path])
     return timestamps
 
+def get_size(start_path = '.'):
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(start_path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            total_size += os.path.getsize(fp)
+    return total_size
+
 def bytes_from_file(filename, chunksize=8192):
     with open(filename, "rb") as f:
         while True:
